638f94c367464f7c44223af2613cc384c5451410
Mathieu Sylvain <mathieu@ti-coco.com>
2012-11-05 15:53:19 -0500

    Update src/enhance.js
    Bugfix for supporting id's, scope, etc.

--- src/enhance.js
+++ src/enhance.js
@@ -39,7 +39,6 @@
 	var hasConsole = typeof console !== "undefined",
 		hasConsoleTime = hasConsole && typeof console.time !== "undefined",
 		enhancements = [],
-		enhancementsById = {},
 		enhancementGroups = {global:[]},
 		enhanceCnt = 0, // Use for auto id
 		enhOptions = {
@@ -51,45 +50,46 @@
 	if (!hasConsole){
 		console = {assert:function(){},clear:function(){},count:function(){},debug:function(){},dir:function(){},dirxml:function(){},error:function(){},exception:function(){},group:function(){},groupCollapsed:function(){},groupEnd:function(){},info:function(){},log:function(){},memoryProfile:function(){},memoryProfileEnd:function(){},profile:function(){},profileEnd:function(){},table:function(){},time:function(){},timeEnd:function(){},timeStamp:function(){},trace:function(){},warn:function(){}};
 	}
+
 	function getGroupeContext(gc) {
 		if(typeof gc == "undefined" || gc.length == 0 || gc[0] == "") return false;
 		gc = (!$.isArray(gc) ? $.trim(gc).split(/\s+/) : gc);
 		if(gc[0] === "*") gc[0] = "global";
 		return gc;
-	};
+	}
 	
 	function applyEnhancements() {
 		var target = this,
-			_enG = enhancementGroups,
+			group,
 			elems,
 			execGroups = {},
 			enhs = {};
 		
-		//console.log(elems);
 		if (target.hasClass(enhOptions["class"])) {
-			elems = target;
-			elems.add(target.find('.'+enhOptions["class"]));
+			elems = target.find(target.find('.'+enhOptions["class"]));
 		} else {
-			elems = target.find('.'+enhOptions["class"]);
+			elems = target.find("[data-enhance]");
 		}
-		
+		elems = elems.add(target);
+
 		$.each(elems, function(index,value) {
 			var g, i, eni;
 			g = getGroupeContext($(this).data(enhOptions.dataHandler) || "");
 			if(g) {
 				for(i=0;g[i];i++){
-					if(typeof _enG[g[i]] !== "undefined" ) {
+					group = enhancementGroups[g[i]];
+					if(typeof group !== "undefined" ) {
 						execGroups[g[i]]=g[i];
-						for(eni=0;_enG[g[i]][eni];eni++) {
+						for(eni=0;group[eni];eni++) {
 							
-							if(!enhs[_enG[g[i]][eni].id]) {
-								enhs[_enG[g[i]][eni].id] = _enG[g[i]][eni] ;
+							if(!enhs[group[eni].id]) {
+								enhs[group[eni].id] = group[eni] ;
 								//reset previously added elements in global scope
-								enhs[_enG[g[i]][eni].id].elems = [];
+								enhs[group[eni].id].elems = [];
 							}
 							
-							if (!!!$(value).data("enhance-"+ _enG[g[i]][eni].id + "-applied")){
-								enhs[_enG[g[i]][eni].id].elems.push(value);
+							if (!!!$(value).data("enhance-"+ group[eni].id + "-applied")){
+								enhs[group[eni].id].elems.push(value);
 							}
 						}
 					}
@@ -108,10 +108,10 @@
 			var id = (this.id !== null) ? "#" + this.id + ": " : "",
 				desc = "Enhanced: " + id + this.title;
 			if (hasConsoleTime) console.time(desc);
+			$(this.elems).data("enhance-"+ this.id + "-applied", true);
+			$(this.elems).addClass("enhance-"+ this.id + "-applied");
+			this.handler($(this.elems), target);
 			try {
-				$(this.elems).data("enhance-"+ this.id + "-applied", true);
-				$(this.elems).addClass("enhance-"+ this.id + "-applied");
-				this.handler(target);
 
 			} catch(e) {
 				if (hasConsole){
@@ -131,9 +131,11 @@
 	function Enhancement(handler, _options) {
 		this.handler = handler;
 		var o = this.option = $.extend({}, _options);
-		this.id = o.id || '__jenhance_id_'+(++enhanceCnt)+'__';
+		this.id = o.id || 'enhancejs-' + (++enhanceCnt);
 		this.title = o.title || "";
-		this.groups = getGroupeContext(o.group);
+		var group = o.group || "";
+		if (o.id) group = group + " " + o.id;
+		this.groups = getGroupeContext(group);
 		this.elems = [];//will contain all elements from the group
 	}
 
@@ -141,7 +143,6 @@
 		if($.isFunction(handler)) {
 			var enh = new Enhancement(handler, _options);
 			enhancements.push(enh);
-			enhancementsById[enh.id] = enh;
 			$.each(enh.groups, function() {
 				var g = this.toString();
 				if(typeof enhancementGroups[g] == "undefined") {
