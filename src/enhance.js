13c2e28dd62255affa7c4b0c5add065b778a5218
Etienne <dionetienne@gmail.com>
2012-11-08 14:02:09 -0500

    Update src/enhance.js

--- src/enhance.js
+++ src/enhance.js
@@ -52,27 +52,24 @@ Upcomming features:
 * -Console log for already enhanced elements 
 * -Option to change the 'enhance' class name to prevent conflict with other enhancing tools
 * -grouped log to clean the console
-* -Fix on the lost of context for the element enhanced
-* -Refactoring
+* 
+* 
 *
 */
 
-(function($) { 
+(function($) {
 	var hasConsole = typeof console !== "undefined",
 		hasConsoleTime = hasConsole && typeof console.time !== "undefined",
 		enhancements = [],
-		enhancementsById = {},
 		enhancementGroups = {global:[]},
 		enhanceCnt = 0, // Use for auto id
-		counter = 0,
-		errors = 0,
-		timers = {},
 		enhOptions = {
 			"class" : "enhance", // default className selector 
-			"dataHandler" : "enhance", // default data attribute [data-enhance]
-			"isEnhancedFlag" : "isEnhance", // default data attribute [data-enhance]
-			"appliedMarkerPrefix" : "isEnhanced-"
+			"dataHandler" : "enhance" // default data attribute [data-enhance]
 		},
+		timers = {},
+		counter = 0,
+		errors = 0;
 		indentForIE = "";
 
 	if(!hasConsoleTime){
@@ -81,7 +78,6 @@ Upcomming features:
 	if (!hasConsole){
 		console = {assert:function(){},clear:function(){},count:function(){},debug:function(){},dir:function(){},dirxml:function(){},error:function(){},exception:function(){},group:function(){},groupCollapsed:function(){},groupEnd:function(){},info:function(){},log:function(){},memoryProfile:function(){},memoryProfileEnd:function(){},profile:function(){},profileEnd:function(){},table:function(){},time:function(){},timeEnd:function(){},timeStamp:function(){},trace:function(){},warn:function(){}};
 	}
-	
 	
 	function logGroup(group, elem, status){
 		elem = elem || "";
@@ -95,15 +91,13 @@ Upcomming features:
 		} else {
 			if(status === "start"){
 				
-				var selector = $(elem).prop("tagName") || "";
+				var selector = $(elem).prop("tagName") || "",
+					id = $(elem).attr("id"),
+					classNames = $(elem).attr("class");
 				
-				
-				var id = $(elem).attr("id");
 				if (id) { 
 					selector += "#"+ id;
 				}
-
-				var classNames = $(elem).attr("class");
 				if (classNames) {
 					selector += "." + $.trim(classNames).replace(/\s/gi, ".");
 				}
@@ -143,157 +137,99 @@ Upcomming features:
 			 console.log(id +" : "+time(id)+"ms");
 		}
 	}
-
+	
 	function getGroupeContext(gc) {
-		if (typeof gc == "undefined" || gc.length == 0 || gc[0] == "") {
-			return false;
-		} 
+		if(typeof gc == "undefined" || gc.length == 0 || gc[0] == "") return false;
 		gc = (!$.isArray(gc) ? $.trim(gc).split(/\s+/) : gc);
-
-		if (gc[0] === "*") {
-			gc[0] = "global";
-		}
-
+		if(gc[0] === "*") gc[0] = "global";
 		return gc;
-	};
-
-
-	function getTargetElems($target) {
-		var elems;
-
-		// Check if the target or one of its decendents has the enhance trigger class
-		// Ex: in $(document).enhance(); "$(document)" is the target.
-		if ($target.hasClass(enhOptions["class"])) {
-			elems = $target;
-			elems.add($target.find('.' + enhOptions["class"]));
-		} else {
-			elems = $target.find('.' + enhOptions["class"]);
-		}
-
-		return elems;
 	}
-
-
-	// TODO: Further refactoring needed
+	
 	function applyEnhancements() {
-
 		var target = this,
-			_enhancementGroups = enhancementGroups,
+			group,
 			elems,
-			enhancementList = {};
-
-		// get list of elements with enhancements
-		elems = getTargetElems(target);
-
-
-		// Determine enhancements to be applied (below), skip elements with enhancement already applied
-		$.each(elems, function(index, curElem) {
-
-
-			var groups, i, j;
-
-			// read data attribute on the element and get list (of the groups) of enhancements to apply
-			groups = getGroupeContext($(this).data(enhOptions.dataHandler) || "");
-
-			// skip if already enhanced
-			if (!$(curElem).data(enhOptions.isEnhancedFlag) === true) {
-				if (groups) {
-
-					$(curElem).data(enhOptions.isEnhancedFlag, true);
-					for (i = 0; groups[i]; i++) {
-
-						if (typeof _enhancementGroups[groups[i]] !== "undefined" ) {
-
-							// TODO: clarify this
-							for (j = 0; _enhancementGroups[groups[i]][j]; j++) {
-
-								if (!enhancementList[ _enhancementGroups[groups[i]][j].id ]) {
-
-									enhancementList[ _enhancementGroups[groups[i]][j].id ] = _enhancementGroups[groups[i]][j];
-									//reset previously added elements in global scope
-									enhancementList[ _enhancementGroups[groups[i]][j].id ].elems = [];
-
-									enhancementList[ _enhancementGroups[groups[i]][j].id ].groupByElem = index;
-
-								} else { 
-									console.log('already applied enhancement', _enhancementGroups[groups[i]][j].id); 
-								}
-
-								if (!!!$(curElem).data(enhOptions["appliedMarkerPrefix"] + _enhancementGroups[groups[i]][j].id)){
-									enhancementList[_enhancementGroups[groups[i]][j].id].elems.push(curElem);
-									enhancementList[_enhancementGroups[groups[i]][j].id ].groupByElem = index;
-								}
-
-
-							}
-
-						} else {
-							if (enhancementsById[groups[i]]){
-								enhancementsById[groups[i]].groups = [ "nogroup_" +i ];
-
-								_enhancementGroups[enhancementsById[groups[i]].group] = enhancementsById[groups[i]];
-
-								enhancementList[ _enhancementGroups[enhancementsById[groups[i]].group] ] = _enhancementGroups[enhancementsById[groups[i]].group];
-
-								if (!!!$(curElem).data(enhOptions["appliedMarkerPrefix"] + _enhancementGroups[enhancementsById[groups[i]].group].id)){
-									_enhancementGroups[enhancementsById[groups[i]].group].elems.push(curElem);
-								}
-								_enhancementGroups[enhancementsById[groups[i]].group].groupByElem = index;
+			execGroups = {},
+			enhs = {};
+		
+		if (target.hasClass(enhOptions["class"])) {
+			elems = target.find(target.find('.'+enhOptions["class"]));
+		} else {
+			elems = target.find("[data-enhance]");
+		}
+		elems = elems.add(target);
+		
+		$.each(elems, function(index,value) {
+			var g, i, eni;
+			g = getGroupeContext($(this).data(enhOptions.dataHandler) || "");
+			if(g) {
+				for(i=0;g[i];i++){
+					group = enhancementGroups[g[i]];
+					
+					if(typeof group !== "undefined" ) {
+						execGroups[g[i]]=g[i];
+						
+						for(eni=0;group[eni];eni++) {
+							
+							if(!enhs[group[eni].id]) {
+								enhs[group[eni].id] = group[eni] ;
+								//reset previously added elements in global scope
+								enhs[group[eni].id].elems = [];
+								enhs[group[eni].id].groupId = index;
 							} else {
-								console.warn('No enhancement "' + groups[i] + '" found.');
+								console.log('already applied enhancement', enhs[group[eni].id]); 
 							}
-
-
+							
+							if (!!!$(value).data("enhance-"+ group[eni].id + "-applied")){
+								enhs[group[eni].id].elems.push(value);
+								enhs[group[eni].id].groupId = index;
+							} else {
+								console.log('already applied enhancement', enhs[group[eni].id]); 
+							}
 						}
-
+					} else {
+						console.warn('No enhancement "' + g + '" found.');
 					}
-
-				} else {
-					console.warn('No enhancement specified on', $(this));
 				}
-
-			} else {
-				// TODO: Refactor. At this point it is assumed the enhancement code below worked and successfully applied the enhancements.
-				console.info('Skipped: already enhanced.');
 			}
-
-
 		});
+
+		/*** DEBUG ***/
+		//console.log('enh groups : ', _enG);
+		//console.log("enhancements to apply : ");
+		//console.dir(enhs);
+		/*** END DEBUG ***/
 
 		logGroup("--- Enhancement Detail ",null, "start");
 		startTimer("--- Enhanced Time");
 		
 		var index=0,
 		groupId = 0;
-		/* Apply enhancements */				
-		$.each(enhancementList, function() {
-
-			var xthis= this, 
-				id = (xthis.id !== null) ? "#" + xthis.id + ": " : "",
-				desc = "Enhanced: " + id + xthis.title;
-
+		
+		$.each(enhs, function() {
+			var id = (this.id !== null) ? "#" + this.id + ": " : "",
+				desc = "Enhanced: " + id + this.title,
+				_this= this;
+			
 			function tryCatch(){
 				try {
-
-					/* Start timing enhancement*/
+					
 					startTimer(indentForIE+indentForIE+desc);
 					
-					$(xthis.elems).data(enhOptions["appliedMarkerPrefix"] + xthis.id, true);
-					$(xthis.elems).addClass(enhOptions["appliedMarkerPrefix"] + xthis.id);
-
-					xthis.handler(target);
-					
-					logTime(indentForIE+indentForIE+desc);
+					//$(_this.elems).data("enhance-"+ _this.id + "-applied", true);
+					$(_this.elems).addClass("enhance-"+ _this.id + "-applied");
+					_this.handler($(_this.elems), target);
 					
 					counter = counter + 1;
-
+					logTime(indentForIE+indentForIE+desc);
+					
 				} catch(e) {
 					errors = errors+1;
-					if (hasConsole) {
-						console.error("Enhancement failed: " + xthis.title);
+					if (hasConsole){
+						console.error("Enhancement failed: " + _this.title);
 						console.dir({
 							"exception": e,
-							"enhancement": xthis,
+							"enhancement": _this,
 							"target": target
 						});
 					}
@@ -301,27 +237,25 @@ Upcomming features:
 			}
 			
 			
-			if(groupId !== xthis.groupByElem){
+			if(groupId !== _this.groupId){
 				
 				logGroup("",null, "end");
 				
-				groupId = xthis.groupByElem; 
-				logGroup(indentForIE+"for element(s) : ",xthis.elems, "start");
+				groupId = _this.groupId; 
+				logGroup(indentForIE+"for element(s) : ",_this.elems, "start");
 				
 				tryCatch();
 
 			} else {
 				if( index === 0 ){
-					logGroup(indentForIE+"for element(s) : ",xthis.elems, "start");
+					logGroup(indentForIE+"for element(s) : ",_this.elems, "start");
 				}
 				tryCatch();
 			}
-
+			
 			index= index+1;
-
-			
-			
-		});
+			 
+		}); 
 		
 		logGroup("",null, "end");
 		logGroup("",null, "end");
@@ -329,39 +263,36 @@ Upcomming features:
 		
 		logTime("--- Enhanced Time");
 		counter =0;
+		
 		return this;
 	}
 
 	function Enhancement(handler, _options) {
-		var _opts = this.option = $.extend({}, _options);
 		this.handler = handler;
-		this.id = _opts.id || '__jenhance_id_'+(++enhanceCnt)+'__';
-		this.title = _opts.title || "";
-		this.groups = getGroupeContext(_opts.group);
-		this.elems = []; //will contain all elements from the group
+		var o = this.option = $.extend({}, _options);
+		this.id = o.id || 'enhancejs-' + (++enhanceCnt);
+		this.title = o.title || "";
+		var group = o.group || "";
+		if (o.id) group = group + " " + o.id;
+		this.groups = getGroupeContext(group);
+		this.elems = [];//will contain all elements from the group
 	}
 
-
-
 	function registerEnhancement(handler, _options) {
-
-		if ($.isFunction(handler)) {
+		if($.isFunction(handler)) {
 			var enh = new Enhancement(handler, _options);
 			enhancements.push(enh);
-			enhancementsById[enh.id] = enh;
-
 			$.each(enh.groups, function() {
-				var group = this.toString();
-				if(typeof enhancementGroups[group] === "undefined") {
-					enhancementGroups[group] = [];
-				} 
-				enhancementGroups[group].push(enh);
+				var g = this.toString();
+				if(typeof enhancementGroups[g] == "undefined") {
+					enhancementGroups[g] = [];
+				}
+				enhancementGroups[g].push(enh);
 			});
 		} else {
 			enhOptions = $.extend(enhOptions, handler);
 		}
 	}
-
 
 	function array_merge(first, second, byVal) {
 		if(typeof byVal !== "undefined" && byVal) {
@@ -374,7 +305,7 @@ Upcomming features:
 						first[i++] = second[j];
 					}
 				}
-
+	
 			} else {
 				while ( second[j] !== undefined ) {
 					if($.inArray(second[j], first)===-1) {
@@ -382,20 +313,18 @@ Upcomming features:
 					}
 				}
 			}
-
+	
 			first.length = i;
-
+	
 			return first;
 		} else {
 			return $.merge(first, second);
 		}
 	}
-
 	// Check if jQuery is loaded
 	if ($) {
 		$.fn.array_merge = $.array_merge = array_merge;
 		$.fn.enhance = applyEnhancements;
 		$.enhance = registerEnhancement;
-
-	} 
+	}
 })(jQuery);
