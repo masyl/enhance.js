fc88909f39f75304bf4ce200fd7f936742996211
Etienne <dionetienne@gmail.com>
2013-02-10 02:15:00 -0500

    fix comments

--- src/enhance.js
+++ src/enhance.js
@@ -8,43 +8,43 @@
 * 				Etienne Dion - etienne.dion@nurun.com
 *
 * Modified Date : November 8, 2012
-	Enhance.js
-
-	A javascript library for progressive enhancement
-
-	Usage:
-		 // Apply all enhancements to the whole document
-		jQuery(document).enhance();
-
-		 // Apply all enhancements to a specific part of the page (after ajax or dhtml)
- 		jQuery("#pageSection1").enhance();
-
-		// Register a new enhancement by id
-		jQuery.enhance(function (targets) {
-			// some code here...
-		}, {
-			id: "ajaxPagingBehavior",
-			title: "adding ajax behavior on paging"
-		});
-
-		// Register a new enhancement by group
-		jQuery.enhance(function (targets) {
-			// some code here...
-		}, {
-			id: "ajaxPagingBehavior",
-			title: "adding ajax behavior on paging"
-			group: "ajax"
-		});
-		
-		// Clear Enhancement for this element
-		jQuery("#element").clearEnhance();
-		
-
-
-Upcomming features:
-- Provide a callback for when enhancement are complete
-- Specify a method to test if requirements are met
-
+*	Enhance.js
+*
+*	A javascript library for progressive enhancement
+*
+*	Usage:
+*		 // Apply all enhancements to the whole document
+*		jQuery(document).enhance();
+*
+*		 // Apply all enhancements to a specific part of the page (after ajax or dhtml)
+* 		jQuery("#pageSection1").enhance();
+*
+*		// Register a new enhancement by id
+*		jQuery.enhance(function (targets) {
+*			// some code here...
+*		}, {
+*			id: "ajaxPagingBehavior",
+*			title: "adding ajax behavior on paging"
+*		});
+*
+*		// Register a new enhancement by group
+*		jQuery.enhance(function (targets) {
+*			// some code here...
+*		}, {
+*			id: "ajaxPagingBehavior",
+*			title: "adding ajax behavior on paging"
+*			group: "ajax"
+*		});
+*		
+*		// Clear Enhancement for this element
+*		jQuery("#element").clearEnhance();
+*		
+*
+*
+* Upcomming features:
+* - Provide a callback for when enhancement are complete
+* - Specify a method to test if requirements are met
+*
 * October 13, 2011 Update - AP
 * Added "elems" attribute of the enhancement object which is an array of the 
 * elements where the same data-enhance attribute is applied.
